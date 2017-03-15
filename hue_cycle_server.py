2f9548ed8ffcc464fc714c328568a5e24eb80c05
Bradley Powers <bpowers@locusrobotics.com>
2017-03-15 17:28:41 -0400

    Update with more comments for readability, plus moving to msgpack based system for serialization.

--- hue_cycle_server.py
+++ hue_cycle_server.py
@@ -4,17 +4,28 @@
 # anyone who is listening.
 #----------------------------------------------------------
 
+# Comms libraries, zmq for socket communications, msgpack for message binarization
 import zmq
+import msgpack
+
+# library for color conversion
 import colorsys
-import random
+
+# library for sleeping
 import time
+
+# convenient way to make a cycle, like a list but that wraps around
 from itertools import cycle
+
+# for commandline args
 import sys
-import json
+
+# libraries for printing pretty things to the terminal
 import sparkline
 from termcolor import colored
 
 def print_sparkline(r, g, b):
+    # prints a sparkline, a small graph which shows the color levels of R, G, and B
     spark = sparkline.sparkify([r, g, b])#.encode('utf-8')
     r = round(r/(255/5.))
     g = round(g/(255/5.))
@@ -27,11 +38,12 @@ def print_sparkline(r, g, b):
           '\x1b[38;5;%dm' % color_code, shape
 
 def print_publish_line(topic_name, r, g, b):
+    # Prints a line out to the terminal, along with our sparkline
     print "\033c", "Sending on topic %s:" % topic_name
     print_sparkline(r, g, b)
 
 
-
+# Set up our publisher.  We'll publish to anyone listening on tcp port 5556.
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:5556")
@@ -41,19 +53,25 @@ n = float(sys.argv[1])
 huerange = range(1, int(n))
 hues = [hue/(n*1.0) for hue in huerange]
 huecycle = cycle(hues)
+
 while True:
+    #grab the next hue, convert it to rgb
     hue = huecycle.next()
-    r_float, g_float, b_float = colorsys.hsv_to_rgb(hue, 1, 1)
-    r = (int(r_float*255))
-    g = (int(g_float*255))
-    b = (int(b_float*255))
-    topic_name = "foo"
-    print "\033c"
-    print "Sending on topic %s:" % topic_name
-    print_sparkline(r, g, b)
-    data = json.dumps({"pixels": [[(r, g, b)]*8]*4})
-    #print "Topic: %s Data: %s " %( topic_name, data)
-    socket.send_string("%s %s" % (topic_name, data))
+    rgb = colorsys.hsv_to_rgb(hue, 1, 1)
+    #convert [0,1] float color values to [0,255] integer values.
+    r, g, b = (int(float_value*255) for float_value in rgb)
+    #set up our message packet
+    topic_name = "blinken"
+    # copy our chosen color across all of the unicorn leds
+    pixels = [[(r, g, b)]*8]*4 
+    data  = {"pixels": pixels}
+    bitstring = msgpack.packb(data)
+    #print to the terminal
+    print_publish_line(r, g, b)
+    #data = json.dumps({"pixels": pixels})
+    # send the packet
+    socket.send_string("%s %s" % (topic_name, bitstring))
+    # sleep for a frame
     time.sleep(1/n)
 
 
--- rgbclient.py
+++ rgbclient.py
@@ -8,30 +8,31 @@ import sys
 import zmq
 import unicornhat as unicorn
 import colorsys
-import json
+import msgpack
 
 #  Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
+# Connect to a machine (presently hardcoded to Brad's laptop
 socket.connect("tcp://vulpix.lowellmakes.lan:5556")
 
-topicfilter = "foo"
+# Only listen to a particular topic
+topicfilter = "blinken"
 socket.setsockopt_string(zmq.SUBSCRIBE, \
                          topicfilter.decode('ascii'), \
                          encoding="utf-8")
-
+# Set up our unicorn hat
 unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(0)
 unicorn.brightness(0.5)
 
 
 while True:
-    string = socket.recv_string()
-    splits = string.split()
-    topic = splits[0]
-    data = json.loads(' '.join(string.split()[1:]))
-    pixels = data["pixels"]
+    # receive and unpack our message
+    data = socket.recv_string()
+    message = msgpack.unpackb(data)
+    pixels = messsage["pixels"]
     unicorn.set_pixels(pixels)
     unicorn.show()
     
