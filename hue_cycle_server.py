9cf1c72b503320f331bafc2fd8369a37c96711e0
Bradley Powers <bpowers@locusrobotics.com>
2017-03-14 20:51:40 -0400

    Add pretty terminal display of what is being sent

--- hue_cycle_server.py
+++ hue_cycle_server.py
@@ -11,6 +11,26 @@ import time
 from itertools import cycle
 import sys
 import json
+import sparkline
+from termcolor import colored
+
+def print_sparkline(r, g, b):
+    spark = sparkline.sparkify([r, g, b])#.encode('utf-8')
+    r = round(r/(255/5.))
+    g = round(g/(255/5.))
+    b = round(b/(255/5.))
+    color_code = 16 + (r * 36) + (g * 6) + b
+    shape = u'\u25CF'
+    print colored(spark[0], 'red'), \
+          colored(spark[1], 'green'), \
+          colored(spark[2], 'blue'), \
+          '\x1b[38;5;%dm' % color_code, shape
+
+def print_publish_line(topic_name, r, g, b):
+    print "\033c", "Sending on topic %s:" % topic_name
+    print_sparkline(r, g, b)
+
+
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
@@ -28,7 +48,12 @@ while True:
     g = (int(g_float*255))
     b = (int(b_float*255))
     topic_name = "foo"
+    print "\033c"
+    print "Sending on topic %s:" % topic_name
+    print_sparkline(r, g, b)
     data = json.dumps({"pixels": [[(r, g, b)]*8]*4})
-    print "Topic: %s Data: %s " %( topic_name, data)
+    #print "Topic: %s Data: %s " %( topic_name, data)
     socket.send_string("%s %s" % (topic_name, data))
     time.sleep(1/n)
+
+
